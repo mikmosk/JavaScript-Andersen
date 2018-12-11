f16ae6afd0f13dca71b2caedb89b38163f926a5e
mikmosk <39431567+mikmosk@users.noreply.github.com>
2018-12-11 22:47:30 +0300

    Update init.js

--- 2. Weather forecast - fetch api/js/init.js
+++ 2. Weather forecast - fetch api/js/init.js
@@ -1,5 +1,11 @@
 var submit = document.querySelector('#submit-city');
-	submit.addEventListener('click', function() {	
-	var dataModule = createDataModule();	
-	dataModule.getData();
+	submit.addEventListener('click', function() {		
+		var dataModule = createDataModule();	
+		var showDataModule = createShowDataModule();
+	
+		var dataState = dataModule.getData();
+		dataState .then(data => {
+			showDataModule.showData(data);
+		})
+		.catch(err => showDataModule.showError());	
 });
